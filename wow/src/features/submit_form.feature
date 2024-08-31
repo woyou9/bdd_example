7169c67496e9b30a727833e57e99352ed82742f2
woyou9 <124035523+woyou9@users.noreply.github.com>
2024-08-31 04:46:50 +0200

    Rozszerzenie asercji o sprawdzenie czy wprowadzone w pola wartości są widoczne na podsumowaniu

--- wow/src/features/submit_form.feature
+++ wow/src/features/submit_form.feature
@@ -4,8 +4,9 @@
                 Given the user is on the practice form page
                 When the user fills "Łukasz" in the required first name field
                 And the user fills "Łojewski" in the required last name field
-                And the user selects gender radio button
+                And the user selects "Male" gender radio button
                 And the user fills "5091509500" in the required mobile number field
                 And the user uploads profile picture file
                 And the user presses submit button
                 Then the modal window with form summary should be visible
+                And the form summary should contain filled fields values such as "Łukasz", "Łojewski", "5091509500" and "Male"
--- wow/src/step_definitions/test_fill_the_form.py
+++ wow/src/step_definitions/test_fill_the_form.py
@@ -1,8 +1,14 @@
+import os
 import pytest
 from playwright.sync_api import expect
 from pytest_bdd import scenario, scenarios, when, then, given, parsers
 from wow.src.pages.page_objects.practice_form_page import PracticeFormPage
 from wow.src.step_definitions.conftest import MODAL_HEADER_TEXT
+
+
+@pytest.fixture
+def shared_context():
+    return {}
 
 
 @pytest.mark.normal_scenario_usage
@@ -26,9 +32,9 @@ def fill_required_last_name_field(practice_form_page: PracticeFormPage, last_nam
     practice_form_page.fill_last_name_field(last_name)
 
 
-@when(parsers.parse('the user selects gender radio button'))
-def select_gender(practice_form_page: PracticeFormPage):
-    practice_form_page.gender_radio_buttons.first.click()
+@when(parsers.parse('the user selects {gender} gender radio button'))
+def select_gender(practice_form_page: PracticeFormPage, gender):
+    practice_form_page.page.locator(f'.custom-radio [value={gender}]').click(force=True)
 
 
 @when(parsers.parse('the user fills "{mobile_number}" in the required mobile number field'))
@@ -37,8 +43,10 @@ def fill_required_last_name_field(practice_form_page: PracticeFormPage, mobile_n
 
 
 @when(parsers.parse('the user uploads profile picture file'))
-def upload_picture(download_file, practice_form_page: PracticeFormPage):
-    practice_form_page.upload_profile_picture(download_file)
+def upload_picture(download_file, practice_form_page: PracticeFormPage, shared_context):
+    file_path = download_file
+    shared_context['file_path'] = file_path
+    practice_form_page.upload_profile_picture(file_path)
 
 
 @when(parsers.parse('the user presses submit button'))
@@ -50,5 +58,12 @@ def press_submit(practice_form_page: PracticeFormPage):
 def check_for_modal_window_header(practice_form_page):
     expect(practice_form_page.thanks_for_submitting_header).to_be_visible()
     expect(practice_form_page.thanks_for_submitting_header).to_contain_text(MODAL_HEADER_TEXT)
-    practice_form_page.page.locator('#closeLargeModal').click()
-    practice_form_page.page.reload()
+
+
+@then(parsers.parse('the form summary should contain filled fields values such as "{first_name}", "{last_name}", "{mobile_number}" and "{gender}"'))
+def assert_values_in_form_summary(practice_form_page: PracticeFormPage, first_name, last_name, mobile_number, gender, shared_context):
+    expect(practice_form_page.student_name_table_cell_value).to_have_text(f'{first_name} {last_name}')
+    expect(practice_form_page.mobile_number_table_cell_value).to_have_text(mobile_number)
+    expect(practice_form_page.gender_table_cell_value).to_have_text(gender)
+    expect(practice_form_page.picture_name_table_cell_value).to_have_text(os.path.basename(shared_context.get('file_path')))
+    practice_form_page.close_summary_modal_button.click()
