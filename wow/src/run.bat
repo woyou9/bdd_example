b68b6b84eadc9fd7cc93520f253d3ee29294b480
woyou9 <124035523+woyou9@users.noreply.github.com>
2024-09-05 23:05:41 +0200

    run.bat

--- wow/src/run.bat
+++ wow/src/run.bat
@@ -1,4 +1,6 @@
 call C:\Users\and1_\PycharmProjects\PlaywrightFreestyle\venv\Scripts\activate.bat
 pytest
 start cmd /k "allure serve .\allure_report_results"
+timeout /t 10
+taskkill /f /IM "java.exe"
 deactivate
