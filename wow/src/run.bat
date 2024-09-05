53af784b733ffa8cff7d49321eb20535c333e9df
woyou9 <124035523+woyou9@users.noreply.github.com>
2024-09-05 21:57:39 +0200

    run.bat

--- wow/src/run.bat
+++ wow/src/run.bat
@@ -1,5 +1,4 @@
 call C:\Users\and1_\PycharmProjects\PlaywrightFreestyle\venv\Scripts\activate.bat
 pytest
 start cmd /k "allure serve .\allure_report_results"
-tasklist /FI "IMAGENAME eq java.exe"
 deactivate
